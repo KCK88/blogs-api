23f65e8cbc57ba79c6adaac4410f4ccf470f9045
Claudio Meira <claudiomeirabs@gmail.com>
2024-09-02 16:20:37 -0300

    Requisito 1 feito e testado.

---
+++ src/migrations/20240901174548-users.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('users', { 
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      displayName: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        field: 'display_name',
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      image: {
+        type: Sequelize.STRING
+      }
+     });
+
+  },
+
+  async down (queryInterface, Sequelize) {
+     await queryInterface.dropTable('users');
+  }
+};
---
+++ src/migrations/20240901174637-posts_categories.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('posts_categories', { 
+      postId: {
+        field: 'post_id',
+        type: Sequelize.INTEGER,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        primaryKey: true,
+        references: {
+          model:'blog_posts',
+          key: 'id',
+        },
+        },
+      categoryId: {
+        type: Sequelize.INTEGER,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        primaryKey: true,
+        field: 'category_id',
+        references: {
+          model: 'categories',
+          key: 'id',
+        },
+        }
+     });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('users');
+  }
+};
