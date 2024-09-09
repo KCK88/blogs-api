03055bf0fe7b05791aff3f367f314fec6ff2592a
Claudio Meira <claudiomeirabs@gmail.com>
2024-09-09 20:24:39 -0300

    Correção de erro no token

--- src/middlewares/JWT.js
+++ src/middlewares/JWT.js
@@ -5,7 +5,7 @@ const secret = process.env.JWT_SECRET || 'seusecretdetoken';
 const jwtCreate = (userLogin) => {
   const jwtConfig = { expiresIn: '7d', algorithm: 'HS256' };  
   
-  const token = jwt.sign({ data: { userId: userLogin.dataValues.id } }, secret, jwtConfig);
+  const token = jwt.sign({ data: { userId: userLogin.id } }, secret, jwtConfig);
   return token;
 };
 module.exports = {
